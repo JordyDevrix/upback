be50eb4ce611fdf41f4a247302440819dae65f7d
Jordy <contact@jordydevrix.com>
2026-02-03 18:18:45 +0100

    workflow update

--- src/upback/app.py
+++ src/upback/app.py
@@ -169,6 +169,7 @@ def index(path):
 
 def main():
     import argparse
+    exit(0)
     parser = argparse.ArgumentParser()
     parser.add_argument("--port", "-p", type=int, default=8080)
     args = parser.parse_args()
